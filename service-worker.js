d1d5269b809e0bc4764c50098af47a0dab14f774
Jiro-tt <zra217949@gmail.com>
2026-08-20 13:19:01 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,1 @@
-self.addEventListener("install",(event)=>{
-  event.waitUntil(
-    caches.open('pwa-cache').then((cache)=>{
-      return caches.addAll([
-        ])
-    })
-  )
-});
-self.addEventListener("fetch",(event)=>{
 
-});
-  
