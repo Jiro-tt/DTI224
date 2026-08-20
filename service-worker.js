dce56267c29f3c0a9020f7760da2b17c1ab1b3d9
Jiro-tt <zra217949@gmail.com>
2026-08-20 13:29:56 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install",(event)=>{
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache)=>{
+      return caches.addAll([
+        'jiro-tt.github.io/DTI224/'.
+        'index.html',
+        'style.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch",(event)=>{
+  event.respondWith(
+    caches.match(event,request).then((response)=>{
+      return response || fetch(event,request);
+    })
+  );
+});
