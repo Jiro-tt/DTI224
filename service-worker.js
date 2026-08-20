8de1cd1d1cde7439eb73893f9707c10268af71d3
Jiro-tt <zra217949@gmail.com>
2026-08-20 14:16:57 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
-self.addEventListener("install",(event)=>{
-  event.waitUntil(
-    caches.open('pwa-cache').then((cache)=>{
-      return caches.addAll([
-        'https://jiro-tt.github.io/DTI224/'.
-        'index.html',
-        'style.css'
-      ]);
-    })
-  );
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("pwa-cache").then((cache) => {
+            return cache.addAll([
+                "/DTI224/",
+                "/DTI224/index.html",
+                "/DTI224/style.css"
+            ]);
+        })
+    );
 });
 
-self.addEventListener("fetch",(event)=>{
-  event.respondWith(
-    caches.match(event,request).then((response)=>{
-      return response || fetch(event,request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
